ebb60f96f18c083894b6ed08c7ebcd5b023b29e3
wucaleb <ccaleb1109@gmail.com>
2025-05-25 16:57:07 +0800

    新增輸入一整數判斷為奇數或偶數

--- 6/6h/6-1h.c
+++ 6/6h/6-1h.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
 int main(void){
 //6-1簡易判斷字母或數字
-    /*
     char number;
+    printf("判斷字母或數字:\n");
     scanf("%c",&number);
     if(number>='0' && number<='9')
         printf("此字元是數字\n");
     else
         printf("此字元是字母\n");
-        */
-//6-2判斷此數大於0，等於0，或小於０
+    
+    
+        getchar();
+    printf("請按 Enter 繼續...\n");
+    getchar(); 
+
+    //6-2判斷此數大於0，等於0，或小於０
     int num;
+    printf("輸入一數字，判斷此數大於0，等於0，或小於0\n");
     scanf("%d",&num);
     
     if (num>0)
@@ -22,6 +28,22 @@ int main(void){
     else
     printf("此數小於0\n");
     
+
+    getchar();
+    printf("請按 Enter 繼續...\n");
+    getchar(); 
+
+    //輸入一整數判斷為奇數或偶數
+    int num2;
+    printf("輸入一整數判斷為奇數或偶數\n");
+    scanf("%d",&num2);
+
+    if(num2 % 2== 0)
+        printf("此數為偶數\n");
+    else
+        printf("此數為奇數\n");
+
+
     
     
 
