48ccc6196ef47e9ee95fcdda5e39598ba01c2243
wucaleb <ccaleb1109@gmail.com>
2025-06-07 18:01:04 +0800

    用whlile的方法

--- 7/9x9.c
+++ 7/9x9.c
@@ -4,8 +4,24 @@ int main(void)
     int i ,j;
     for(i = 1; i <= 9; i++){
         for (j = 1; j <= 9; j++)
-        printf("  %dX%d=%2d" ,i, j, i*j);
+        printf("  %dX%d=%02d" ,i, j, i*j);
         printf("\n");
     }
+    i = 1;
+    while (i <= 9 )
+    {
+        j = 1;
+        while (j <= 9)
+        {    
+            
+            printf("  %dX%d=%2d ",i,j,i*j);
+            j++; 
+        }
+        printf("\n");
+        i++;
+    }
+    
     return 0;
+
+    
 }
