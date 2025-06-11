ce4a925603bf1dd5b8ba3c3bb155e955d688d63f
wucaleb <ccaleb1109@gmail.com>
2025-06-11 16:12:36 +0800

    8-h7.c做出呼叫函數找出第x的質數是？

--- 8/homework/8-3/7.c
+++ 8/homework/8-3/7.c
@@ -1,24 +1,27 @@
 #include<stdio.h>
-int main (){
-    int n, count = 0;
-    scanf("%d", &n);
-
-
+int prime(int n){
+    int count = 0;
     for(int i = 2; ; i++){
         int isPrime = 1;
-        for(int j = 2; j*j <= i; j++){
-        if(i % j == 0){
-                isPrime = 0;
-                break;
+            for(int j = 2; j*j <= i; j++){
+            if(i % j == 0){
+            isPrime = 0;
+            break;
             }
         }
     if(isPrime){
         count++;
-        if(count == n){
-        printf("第%d個質數是:%d\n", n, i);
-        break;
+            if(count == n){
+            return i;
+            }
         }   
     }
-}   
+}
+int main (){
+    int x;
+    printf("輸入你要找的質數是第幾個:\n");
+    scanf("%d", &x);
+    int result = prime(x);
+    printf("第%d 個質數是：%d\n", x, result);
     return 0;
 }
