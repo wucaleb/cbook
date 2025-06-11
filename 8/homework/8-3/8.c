a1b9a0a16fd229c85d1edca7148b4febf9801816
wucaleb <ccaleb1109@gmail.com>
2025-06-11 22:52:31 +0800

    8-8.c

--- 8/homework/8-3/8.c
+++ 8/homework/8-3/8.c
@@ -3,9 +3,9 @@ double f(double x){
     return 3 * x * x * x + 2 * x - 1;
 }
 int main(){
-    printf("f(-3.2)   = %.6f\n", f(-3.2));
-    printf("f(-2.1)   = %.6f\n", f(-2.1));
-    printf("f(0)   = %.6f\n", f(0));
-    printf("f(2.1) = %.6f\n", f(2.1));
+    printf("f(-3.2) = %.6f\n", f(-3.2));
+    printf("f(-2.1) = %.6f\n", f(-2.1));
+    printf("f(0.00) = %.6f\n", f(0));
+    printf("f(2.10) = %.6f\n", f(2.1));
     return 0;
 }
