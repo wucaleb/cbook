afa5fa933a36366dd67fe1dff21a604bc50f7bc2
wucaleb <ccaleb1109@gmail.com>
2025-10-13 14:47:55 +0800

    首次函數範例

---
+++ 8/prog/8_1.c
@@ -0,0 +1,17 @@
+//函數範例 void star(void)
+#include<stdio.h>
+void star(void);
+int main(void)
+{
+    star();
+    printf("welcome to C langue\n");
+    star();
+
+    return 0;
+}
+
+void star(void)
+{
+    printf("*******************\n");
+    return;
+}
