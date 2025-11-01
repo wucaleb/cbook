b8e08f7b9432ebe406d1f6131c872770c76b7140
wucaleb <ccaleb1109@gmail.com>
2025-11-01 22:02:34 +0800

    二維陣列與雙重指標範例與應用”

--- 10/prog/10_22.c
+++ 10/prog/10_22.c
@@ -1,7 +1,24 @@
-/**/
+/*印出陣列位置*/
 #include<stdio.h>
 int main(void)
 {
+    int num[3][4];
+
+    printf("num=%p\n" ,num);
+    printf("&num=%p\n" ,&num);
+    printf("*num=%p\n",*num);
+    printf("\n");
+
+    printf("num[0]=%p\n",num[0]);
+    printf("num[1]=%p\n",num[1]);
+    printf("num[2]=%p\n",num[2]);
+    printf("\n");
+
+    printf("&num[0]=%p\n",&num[0]);
+    printf("&num[1]=%p\n",&num[1]);
+    printf("&num[2]=%p\n",&num[2]);
+    printf("\n");
+
 
     return 0;
 }
